447712173fd05bdd6731ea7b7c6040212fa3b515
Hafte Nigus Tsadik <haftenigus@gmail.com>
2021-11-20 18:56:10 +0300

    Missed Model Files Added

---
+++ app/Models/ContactInfo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasPermission;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Auditable as AuditableTrait;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class ContactInfo extends Authorizable implements Auditable
+{
+    use HasFactory, HasPermission, AuditableTrait;
+}
